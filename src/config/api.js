3aa2973c58c6ae4c4c103dc310255136042d3a94
ShelbyE <60080830+Shelby219@users.noreply.github.com>
2020-11-18 09:57:32 +1000

    changed based URL

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // Create an axios instance
 export default axios.create({
-  baseURL: 'http://localhost:3009',
+  baseURL: 'https://peaceful-beach-10735.herokuapp.com',
   timeout: 5000,
   withCredentials: true
 })
